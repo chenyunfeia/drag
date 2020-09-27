890974c3da37e37c7d49acf60d9c651404b0761d
chenyunfeia <1732851717@qq.com>
2020-09-27 11:52:44 +0800

    two

--- src/components/asideLeft.vue
+++ src/components/asideLeft.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="asideLeft">aaa</div>
+  <div id="asideLeft">aaa</div>
 </template>
 
 <style lang="scss" scoped>
-#asideLeft{
-    width: 300px;
-    height: 100%;
-    background-color: red;
+#asideLeft {
+  width: 300px;
+  height: 100%;
+  background-color: red;
 }
 </style>
